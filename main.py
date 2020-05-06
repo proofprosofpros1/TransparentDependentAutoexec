ddb62d014ebfecd86a9ad0b7acc3a50d6e07544c
proofprosofpros1 <andrew.t.yoon@gmail.com>
2020-05-06 01:43:22 +0000

    added senior option, changed random to 1-100

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import random
 a = "Andrew"
 print("Hello " + a)
 
-age = 13
-if age > 12:
-  print("teen")
-else:
-  print("preteen")
-age = random.randint(10,20)
+
+age = random.randint(1,100)
 if age > 12 and age < 20:
   print("teen")
-elif age == 20:
-  print("not teen")
+elif age >= 20 and age < 65:
+  print("adult")
+elif age >= 65:
+  print("senior")
 else:
   print("preteen")
